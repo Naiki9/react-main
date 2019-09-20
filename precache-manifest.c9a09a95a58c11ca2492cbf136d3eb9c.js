c1c518f3b2d05adb9d79c2a26c8cfacb391ebe1a
Naiki9 <olabisi94@gmail.com>
2019-09-20 18:10:46 +0100

    Updates

---
+++ precache-manifest.c9a09a95a58c11ca2492cbf136d3eb9c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108c32235628625b176d5abfaff08e68",
+    "url": "/react-main/index.html"
+  },
+  {
+    "revision": "ef5a35430b42d748604d",
+    "url": "/react-main/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2907772be22d73bed7aa",
+    "url": "/react-main/static/js/2.72105dd9.chunk.js"
+  },
+  {
+    "revision": "ef5a35430b42d748604d",
+    "url": "/react-main/static/js/main.b77b0fbd.chunk.js"
+  },
+  {
+    "revision": "8a143c15f8a1c5d312cb",
+    "url": "/react-main/static/js/runtime~main.9bb2a0da.js"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/react-main/static/media/bootstrap.73844123.png"
+  },
+  {
+    "revision": "1651ac0d07bb9054124d93b16971d341",
+    "url": "/react-main/static/media/css.1651ac0d.png"
+  },
+  {
+    "revision": "1706c9f16bd08eb5e03f1df3e0a94a1c",
+    "url": "/react-main/static/media/figma.1706c9f1.png"
+  },
+  {
+    "revision": "a66b9be19ec11b5cc58304f60e924fcc",
+    "url": "/react-main/static/media/header-bg.a66b9be1.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/react-main/static/media/header-bgc.e2d2a51c.jpg"
+  },
+  {
+    "revision": "ca522180f25dc88e135c54a1d43c78fd",
+    "url": "/react-main/static/media/ionic.ca522180.png"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/react-main/static/media/java.652fdb65.png"
+  },
+  {
+    "revision": "e068a7fe43fbf17c16de64f26c5e7ccc",
+    "url": "/react-main/static/media/javascript.e068a7fe.png"
+  },
+  {
+    "revision": "076aee3cc79294eca8643a1c05a42e4d",
+    "url": "/react-main/static/media/mdb.076aee3c.png"
+  },
+  {
+    "revision": "6d390c1e894a80125b2b0ae77d11ae97",
+    "url": "/react-main/static/media/php.6d390c1e.png"
+  },
+  {
+    "revision": "88b5e0ec0c5bdd59e7a1362add84bb57",
+    "url": "/react-main/static/media/react.88b5e0ec.png"
+  },
+  {
+    "revision": "b01ac5a00a024c39f9a1be74589cbfd9",
+    "url": "/react-main/static/media/uiux.b01ac5a0.jpeg"
+  },
+  {
+    "revision": "bb85524f60a0974f372f9d01a1c31ce7",
+    "url": "/react-main/static/media/webdesign.bb85524f.jpeg"
+  },
+  {
+    "revision": "7569ea2e31240b425a74dc11f83c6bdf",
+    "url": "/react-main/static/media/webdev.7569ea2e.jpeg"
+  }
+]);
